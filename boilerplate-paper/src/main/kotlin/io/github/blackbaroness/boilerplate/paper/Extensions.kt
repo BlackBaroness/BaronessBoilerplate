6b2997e6a7ff7f5108fe3403bc21e5cb8a0addef
whyzervellasskx <whyzervellasskx@gmail.com>
2025-02-07 19:06:19 +0300

    added pickup and aging parameters

--- boilerplate-paper/src/main/kotlin/io/github/blackbaroness/boilerplate/paper/Extensions.kt
+++ boilerplate-paper/src/main/kotlin/io/github/blackbaroness/boilerplate/paper/Extensions.kt
@@ -78,11 +78,13 @@ fun Inventory.toMap(clone: Boolean = true): Map<Int, ItemStack?> {
         .toMap()
 }
 
-fun Player.giveOrDrop(items: Collection<ItemStack>) {
+fun Player.giveOrDrop(items: Collection<ItemStack>, canPickup: Boolean, willAge: Boolean) {
+    syncOnly()
+
     inventory.addItem(*items.toTypedArray()).forEach { (_, item) ->
         location.world.dropItem(location, item) { droppedItem ->
-            droppedItem.owner = uniqueId
-            droppedItem.setWillAge(false)
+            droppedItem.owner = if (canPickup) uniqueId else null
+            droppedItem.setWillAge(willAge)
         }
     }
 }
