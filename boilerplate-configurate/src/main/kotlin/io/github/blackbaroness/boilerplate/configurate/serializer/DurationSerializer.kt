ce8412352c25f314f760d8e3cf595c9053e6747c
whyzervellasskx <whyzervellasskx@gmail.com>
2025-02-23 04:19:44 +0300

    .

--- boilerplate-configurate/src/main/kotlin/io/github/blackbaroness/boilerplate/configurate/serializer/DurationSerializer.kt
+++ boilerplate-configurate/src/main/kotlin/io/github/blackbaroness/boilerplate/configurate/serializer/DurationSerializer.kt
@@ -19,6 +19,6 @@ class DurationSerializer : TypeSerializer<Duration> {
             return
         }
 
-        node.set(DurationSerializer.serialize(obj, DurationFormats.mediumLengthEnglish()))
+        node.set(DurationSerializer.serialize(obj, DurationFormats.mediumLengthRussian()))
     }
 }
