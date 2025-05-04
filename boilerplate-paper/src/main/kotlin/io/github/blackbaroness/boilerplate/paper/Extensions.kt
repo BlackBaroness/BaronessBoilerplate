7d2121c32455dbae91c9be4b84297e772d3a3591
WhyZerVellasskx <116300577+WhyZerVellasskx@users.noreply.github.com>
2025-05-04 03:53:46 +0300

    feat: provide more event dispatchers

--- boilerplate-paper/src/main/kotlin/io/github/blackbaroness/boilerplate/paper/Extensions.kt
+++ boilerplate-paper/src/main/kotlin/io/github/blackbaroness/boilerplate/paper/Extensions.kt
@@ -24,8 +24,10 @@ import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockEvent
 import org.bukkit.event.entity.EntityEvent
+import org.bukkit.event.inventory.InventoryEvent
 import org.bukkit.event.player.PlayerEvent
 import org.bukkit.event.vehicle.VehicleEvent
+import org.bukkit.event.weather.WeatherEvent
 import org.bukkit.event.world.ChunkEvent
 import org.bukkit.event.world.WorldEvent
 import org.bukkit.inventory.Inventory
@@ -187,6 +189,8 @@ fun <T : Event> findDispatcherForEvent(plugin: Plugin, event: T): CoroutineConte
         is PlayerEvent -> plugin.entityDispatcher(event.player)
         is BlockEvent -> plugin.regionDispatcher(event.block.location)
         is ChunkEvent -> plugin.regionDispatcher(event.world, event.chunk.x, event.chunk.z)
+        is InventoryEvent -> plugin.entityDispatcher(event.view.player)
+        is WeatherEvent -> plugin.globalRegionDispatcher
         is WorldEvent -> plugin.globalRegionDispatcher
         is MCCoroutineExceptionEvent -> plugin.asyncDispatcher // can be called on different threads, IDK what to do
         else -> throw IllegalStateException("Cannot find dispatcher for ${event::class.simpleName}, override it manually")
