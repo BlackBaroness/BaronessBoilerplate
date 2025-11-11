de39554f94f569297a71e0b43dc5bf586ff1fcc1
BlackBaroness <57033600+BlackBaroness@users.noreply.github.com>
2025-11-11 04:37:14 +0200

    fix: change window title in previous/next page buttons

--- boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/invui/button/NextPageButton.kt
+++ boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/invui/button/NextPageButton.kt
@@ -1,51 +1,25 @@
 package io.github.blackbaroness.boilerplate.invui.button
 
-import io.github.blackbaroness.boilerplate.adventure.tagResolver
-import io.github.blackbaroness.boilerplate.base.Boilerplate
 import io.github.blackbaroness.boilerplate.kotlinx.serialization.type.ItemTemplate
-import io.github.blackbaroness.boilerplate.paper.playSound
 import net.kyori.adventure.text.Component
 import org.bukkit.Sound
-import org.bukkit.entity.Player
-import org.bukkit.event.inventory.ClickType
-import org.bukkit.event.inventory.InventoryClickEvent
-import xyz.xenondevs.invui.gui.PagedGui
-import xyz.xenondevs.invui.item.ItemProvider
-import xyz.xenondevs.invui.item.impl.controlitem.ControlItem
-import xyz.xenondevs.invui.window.changeTitle
-import kotlin.math.max
 
 class NextPageButton(
-    private val pagePresentTemplate: ItemTemplate,
-    private val pageAbsentTemplate: ItemTemplate,
-    private val sound: Sound? = null,
-    private val windowTitleChanger: ((currentPage: Int, targetPage: Int) -> Component)? = null,
-) : ControlItem<PagedGui<*>>() {
+    pagePresentTemplate: ItemTemplate,
+    pageAbsentTemplate: ItemTemplate,
+    sound: Sound? = null,
+    windowTitleChanger: ((currentPage: Int, totalPages: Int) -> Component)? = null,
+) : PageButton(pagePresentTemplate, pageAbsentTemplate, sound, windowTitleChanger) {
 
-    override fun getItemProvider(gui: PagedGui<*>): ItemProvider {
-        val item = if (gui.hasNextPage()) pagePresentTemplate else pageAbsentTemplate
-        return item.resolve(
-            Boilerplate.tagResolver("current_page", gui.currentPage + 1),
-            Boilerplate.tagResolver("page_amount", max(1, gui.pageAmount)),
-            Boilerplate.tagResolver("target_page", gui.currentPage + 2),
-        )
+    override val displayedTargetPage: Int
+        get() = gui.currentPage + 2
+
+    override fun canMove(): Boolean {
+        return gui.hasNextPage()
     }
 
-    override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-        if (!clickType.isLeftClick) return
-        if (!gui.hasNextPage()) return
-
+    override fun move() {
         gui.goForward()
-
-        if (windowTitleChanger != null) {
-            val title = windowTitleChanger.invoke(gui.currentPage + 1, gui.currentPage + 2)
-            for (window in windows) {
-                window.changeTitle(title.asComponent())
-            }
-        }
-
-        if (sound != null) {
-            player.playSound(sound)
-        }
     }
+
 }
--- boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/invui/button/PreviousPageButton.kt
+++ boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/invui/button/PreviousPageButton.kt
@@ -1,51 +1,24 @@
 package io.github.blackbaroness.boilerplate.invui.button
 
-import io.github.blackbaroness.boilerplate.adventure.tagResolver
-import io.github.blackbaroness.boilerplate.base.Boilerplate
 import io.github.blackbaroness.boilerplate.kotlinx.serialization.type.ItemTemplate
-import io.github.blackbaroness.boilerplate.paper.playSound
 import net.kyori.adventure.text.Component
 import org.bukkit.Sound
-import org.bukkit.entity.Player
-import org.bukkit.event.inventory.ClickType
-import org.bukkit.event.inventory.InventoryClickEvent
-import xyz.xenondevs.invui.gui.PagedGui
-import xyz.xenondevs.invui.item.ItemProvider
-import xyz.xenondevs.invui.item.impl.controlitem.ControlItem
-import xyz.xenondevs.invui.window.changeTitle
-import kotlin.math.max
 
 class PreviousPageButton(
-    private val pagePresentTemplate: ItemTemplate,
-    private val pageAbsentTemplate: ItemTemplate,
-    private val sound: Sound? = null,
-    private val windowTitleChanger: ((currentPage: Int, targetPage: Int) -> Component)? = null,
-) : ControlItem<PagedGui<*>>() {
+    pagePresentTemplate: ItemTemplate,
+    pageAbsentTemplate: ItemTemplate,
+    sound: Sound? = null,
+    windowTitleChanger: ((currentPage: Int, totalPages: Int) -> Component)? = null,
+) : PageButton(pagePresentTemplate, pageAbsentTemplate, sound, windowTitleChanger) {
 
-    override fun getItemProvider(gui: PagedGui<*>): ItemProvider {
-        val item = if (gui.hasPreviousPage()) pagePresentTemplate else pageAbsentTemplate
-        return item.resolve(
-            Boilerplate.tagResolver("current_page", gui.currentPage + 1),
-            Boilerplate.tagResolver("page_amount", max(1, gui.pageAmount)),
-            Boilerplate.tagResolver("target_page", gui.currentPage),
-        )
+    override val displayedTargetPage
+        get() = gui.currentPage
+
+    override fun canMove(): Boolean {
+        return gui.hasPreviousPage()
     }
 
-    override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-        if (!clickType.isLeftClick) return
-        if (!gui.hasPreviousPage()) return
-
+    override fun move() {
         gui.goBack()
-
-        if (windowTitleChanger != null) {
-            val title = windowTitleChanger.invoke(gui.currentPage + 1, gui.currentPage)
-            for (window in windows) {
-                window.changeTitle(title.asComponent())
-            }
-        }
-
-        if (sound != null) {
-            player.playSound(sound)
-        }
     }
 }
