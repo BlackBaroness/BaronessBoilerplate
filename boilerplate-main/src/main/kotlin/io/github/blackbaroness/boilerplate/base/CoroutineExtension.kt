c9b32593c99a0777161982a1e4f220361bf93fc1
BlackBaroness <57033600+BlackBaroness@users.noreply.github.com>
2026-01-26 19:30:39 +0200

    fix: coroutine delay extensions

--- boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/base/CoroutineExtension.kt
+++ boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/base/CoroutineExtension.kt
@@ -1,60 +1,48 @@
 package io.github.blackbaroness.boilerplate.base
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.time.delay
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
-import kotlin.time.Duration.Companion.ZERO
 import kotlin.time.Duration as KDuration
 
+@JvmName("delayIntRange")
 suspend fun delay(range: IntRange, unit: ChronoUnit) {
-    require(!range.isEmpty()) {
-        "Delay range must not be empty"
-    }
-    require(!(range.first == 0 && range.last == 0)) {
-        "Delay range must not be 0..0"
-    }
-
-    val value = Random.nextInt(range.first, range.last + 1)
-    delay(Duration.of(value.toLong(), unit))
+    delay(range, TimeUnit.of(unit))
 }
 
+@JvmName("delayIntRange")
 suspend fun delay(range: IntRange, unit: TimeUnit) {
-    require(!range.isEmpty()) {
-        "Delay range must not be empty"
-    }
-    require(!(range.first == 0 && range.last == 0)) {
-        "Delay range must not be 0..0"
-    }
-
-    val value = Random.nextInt(range.first, range.last + 1)
-    delay(Duration.of(value.toLong(), unit.toChronoUnit()))
+    delayRandomRange(range) { unit.toMillis(it.toLong()) }
 }
 
+@JvmName("delayLongRange")
+suspend fun delay(range: LongRange, unit: ChronoUnit) {
+    delay(range, TimeUnit.of(unit))
+}
+
+@JvmName("delayLongRange")
+suspend fun delay(range: LongRange, unit: TimeUnit) {
+    delayRandomRange(range) { unit.toMillis(it) }
+}
+
+@JvmName("delayKotlinDuration")
 suspend fun delay(range: ClosedRange<KDuration>) {
-    require(!range.isEmpty()) {
-        "Delay range must not be empty"
-    }
-    require(!(range.start == ZERO && range.endInclusive == ZERO)) {
-        "Delay range must not be 0..0"
-    }
-
-    val from = range.start.inWholeMilliseconds
-    val to = range.endInclusive.inWholeMilliseconds
-    delay(Random.nextLong(from, to + 1))
+    delayRandomRange(range) { it.inWholeMilliseconds }
 }
 
+@JvmName("delayJavaDuration")
 suspend fun delay(range: ClosedRange<Duration>) {
-    require(!range.isEmpty()) {
-        "Delay range must not be empty"
-    }
-    require(!(range.start.isZero && range.endInclusive.isZero)) {
-        "Delay range must not be 0..0"
-    }
+    delayRandomRange(range) { it.toMillis() }
+}
 
-    val from = range.start.toMillis()
-    val to = range.endInclusive.toMillis()
-    delay(Random.nextLong(from, to + 1))
+private suspend inline fun <T : Comparable<T>> delayRandomRange(range: ClosedRange<T>, toMillis: (T) -> Long) {
+    require(!range.isEmpty()) { "Delay range must not be empty" }
+    delayRandomRangeMillis(toMillis(range.start), toMillis(range.endInclusive))
+}
+
+private suspend fun delayRandomRangeMillis(min: Long, max: Long) {
+    require(min < max) { "Invalid delay range: $min..$max" }
+    delay(Random.nextLong(min, max + 1))
 }
