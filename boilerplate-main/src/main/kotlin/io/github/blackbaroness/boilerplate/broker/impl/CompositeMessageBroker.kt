ab954882a7fcf7562a9ca4065ddd850bba568639
whyzervellasskx <whyzervellasskx@gmail.com>
2026-02-04 11:43:08 +0300

    feat: redisson, composite message brokers

---
+++ boilerplate-main/src/main/kotlin/io/github/blackbaroness/boilerplate/broker/impl/CompositeMessageBroker.kt
@@ -0,0 +1,37 @@
+package io.github.blackbaroness.boilerplate.broker.impl
+
+import io.github.blackbaroness.boilerplate.broker.MessageBroker
+import io.github.blackbaroness.boilerplate.broker.ReceivedMessage
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.merge
+import kotlinx.serialization.KSerializer
+import kotlin.reflect.KClass
+
+class CompositeMessageBroker<TRANSPORT>(
+    private val brokers: List<MessageBroker<TRANSPORT>>,
+) : BaseMessageBroker<TRANSPORT>() {
+
+    constructor(vararg brokers: MessageBroker<TRANSPORT>) : this(brokers.toList())
+
+    override suspend fun <MESSAGE : Any> publish(
+        topic: String,
+        message: MESSAGE,
+        messageClass: KClass<MESSAGE>,
+        serializer: KSerializer<MESSAGE>?,
+    ) {
+        brokers.forEach { broker ->
+            broker.publish(topic, message, messageClass, serializer)
+        }
+    }
+
+    override fun <MESSAGE : Any> subscribe(
+        topic: String,
+        messageClass: KClass<MESSAGE>,
+        serializer: KSerializer<MESSAGE>?,
+    ): Flow<ReceivedMessage<TRANSPORT, MESSAGE>> =
+        merge(
+            *brokers.map {
+                it.subscribe(topic, messageClass, serializer)
+            }.toTypedArray()
+        )
+}
